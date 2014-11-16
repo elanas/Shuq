7b8b47124e39463e322cd00f1589c60bf8a9d54d
Joshua Barza <jbarza1@jhu.edu>
2014-11-16 16:29:31 -0500

    Documentation for iOS

---
+++ iOS/html/interface_new_user_view_controller.js
@@ -0,0 +1,8 @@
+var interface_new_user_view_controller =
+[
+    [ "prepareForSegue:sender:", "interface_new_user_view_controller.html#a1d9454dac3ca806f50be139e7070bcc6", null ],
+    [ "model", "interface_new_user_view_controller.html#a385cdff89b60201889619c0d7580bd5b", null ],
+    [ "continueButton", "interface_new_user_view_controller.html#a42f9b121c4993e4c3b345c21958467c7", null ],
+    [ "newpassTextField", "interface_new_user_view_controller.html#a43b682d6420e329e1545ca45bebfd5cf", null ],
+    [ "newuserTextField", "interface_new_user_view_controller.html#a988335554a1aa86bd94d5afc354b45a6", null ]
+];
