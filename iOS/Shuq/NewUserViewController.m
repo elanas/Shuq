14ee5ea633e7353ac68fa33cffed1e0f995cdf55
Elana Stroud <estroud1@jhu.edu>
2014-12-13 16:18:42 -0500

    updated new user screen

--- iOS/Shuq/NewUserViewController.m
+++ iOS/Shuq/NewUserViewController.m
@@ -26,9 +26,49 @@
 
 - (void)viewDidLoad
 {
+    
+    FRAME_SIZE = 45;
+    ANIMATION_SPEED = .3;
+    JUMP = 70;
+    BIG_JUMP = 135;
+    UIColor *placeholderColor = [UIColor colorWithRed:141/255.0 green:150/255.0 blue:164/255.0 alpha:1];
+    
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    //    self.view.backgroundColor = [UIColor clearColor];
+    
+    _newuserTextField.delegate = self;
+    _newpassTextField.delegate = self;
+    _newcontactTextField.delegate = self;
+    _locationField.delegate = self;
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+    
+    //formatting text fields
+    CGRect frameRect = _newuserTextField.frame;
+    frameRect.size.height = FRAME_SIZE;
+    _newuserTextField.frame = frameRect;
+    _newuserTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"username" attributes:@{NSForegroundColorAttributeName: placeholderColor}];
+    
+    
+    frameRect = _newpassTextField.frame;
+    frameRect.size.height = FRAME_SIZE;
+    _newpassTextField.frame = frameRect;
+    _newpassTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"password" attributes:@{NSForegroundColorAttributeName: placeholderColor}];
+    
+    frameRect = _newcontactTextField.frame;
+    frameRect.size.height = FRAME_SIZE;
+    _newcontactTextField.frame = frameRect;
+    _newcontactTextField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"555-123-4567" attributes:@{NSForegroundColorAttributeName: placeholderColor}];
+    
+    frameRect = _locationField.frame;
+    frameRect.size.height = FRAME_SIZE;
+    _locationField.frame = frameRect;
+    _locationField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"enter your zipcode" attributes:@{NSForegroundColorAttributeName: placeholderColor}];
+
+
+    
     [_continueButton addTarget:self action:@selector(checkUsername) forControlEvents:UIControlEventTouchDown];
     
     [_createAccountButton addTarget:self action:@selector(addLocation) forControlEvents:UIControlEventTouchDown];
@@ -57,6 +97,19 @@
         //aware user
         //don't segue
     }
+}
+
+
+-(void)dismissKeyboard {
+    [_newuserTextField resignFirstResponder];
+    [_newpassTextField resignFirstResponder];
+    [_newcontactTextField resignFirstResponder];
+    [_locationField resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField
+{
+    return YES;
 }
 
 -(void)addLocation {
