e639b4f8b794ed74a583d4421d336ef69576bbd3
Elana Stroud <estroud1@jhu.edu>
2014-12-14 19:00:13 -0500

    things work, but first items in list are cut off

---
+++ iOS/Shuq/ListTableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListTableTableViewController.h
+//  Shuq
+//
+//  Created by Elana Stroud on 12/14/14.
+//  Copyright (c) 2014 com.cape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListTableTableViewController : UITableViewController
+
+@end
